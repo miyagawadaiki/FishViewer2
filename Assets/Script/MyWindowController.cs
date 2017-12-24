9534797585615086593cef5277e19832a8691cee
miyagawadaiki <rockenmeier@gmail.com>
2017-12-24 22:01:49 +0900

    function about window-select

---
+++ Assets/Script/MyWindowController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyWindowController : MonoBehaviour {
+	[SerializeField]
+	private Image frameImg = null;
+	[SerializeField]
+	private Color normalColor = Color.white;
+	[SerializeField]
+	private Color selectedColor = Color.white;
+
+	private RectTransform recTra;
+
+	private bool isSelected = false;
+
+	void Awake() {
+		SetNormalMode ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		recTra = this.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetSelectMode() {
+		if (isSelected)
+			return;
+		isSelected = true;
+		frameImg.color = selectedColor;
+	}
+
+	public void SetNormalMode() {
+		if (!isSelected)
+			return;
+		isSelected = false;
+		frameImg.color = normalColor;
+	}
+
+	public int GetSiblingIndex() {
+		return recTra.GetSiblingIndex ();
+	}
+		
+	public void SetAsLastSibling() {
+		recTra.SetAsLastSibling();
+	}
+
+	public bool Contains(Vector2 vec) {
+		Rect rect = recTra.rect;
+		Vector2 tmp = vec - (Vector2)this.transform.position;
+		Vector2 canvas = (Vector2)GameObject.Find ("Canvas").transform.localScale;
+		return tmp.x >= -rect.width * canvas.x / 2 && tmp.x <= rect.width * canvas.x / 2 && tmp.y >= -rect.height * canvas.y / 2 && tmp.y <= rect.height * canvas.y / 2;
+	}
+
+	void OnMouseDown() {
+		// Debug
+		Debug.Log(this.ToString());
+	}
+
+	public bool IsSelect {
+		get { return isSelected; }
+	}
+}
---
+++ Assets/Script/MyWindowManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyWindowManager : MonoBehaviour {
+
+	private List<MyWindowController> windowList;
+
+	void Awake() {
+		windowList = new List<MyWindowController> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Window")) {
+			windowList.Add (obj.GetComponent<MyWindowController> ());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log (Input.mousePosition);
+			OnMouseDown ();
+		}
+	}
+
+	void OnMouseDown() {
+		int max = -1;
+		MyWindowController clicked = null;
+		Vector2 pos = Input.mousePosition;
+		foreach (MyWindowController mwc in windowList) {
+			mwc.SetNormalMode ();
+			if (mwc.Contains(pos)) {
+				Debug.Log ("Contains");
+				if (mwc.GetSiblingIndex () > max) {
+					clicked = mwc;
+					max = mwc.GetSiblingIndex ();
+				}
+			}
+		}
+		if(clicked != null) {
+			clicked.SetAsLastSibling ();
+			clicked.SetSelectMode ();
+		}
+	}
+}
